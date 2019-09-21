3e29982fb51ae8e51d919929be1014d49246beea
negimohit1209 <mohitsngh1997@gmail.com>
2019-09-21 22:27:39 +0530

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import homepage from "./pages/homePage";
+import registerPage from "./pages/registerPage";
+import dashboardPage from "./pages/dashboardPage";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const routes = (
+    <Switch>
+      <Route path="/" component={homepage} exact></Route>
+      <Route path="/register" component={registerPage}></Route>
+      <Route path="/dashboard" component={dashboardPage}></Route>
+      <Redirect to="/"></Redirect>
+    </Switch>
   );
+  return <div className="App">{routes}</div>;
 }
 
 export default App;
